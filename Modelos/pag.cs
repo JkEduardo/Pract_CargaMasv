a7b1f3d8082e0bae4a89dd193467cf6c33c78b98
PC FACTOR WHITE <PC FACTOR WHITE@RAMOS-CE>
2024-09-08 20:12:38 -0600

    Actualización front Listado#

--- Modelos/pag.cs
+++ Modelos/pag.cs
@@ -6,14 +6,16 @@ using System.Threading.Tasks;
 
 namespace Modelos
 {
-        public class UsuarioViewModel
-        {
-#pragma warning disable CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
-            public List<Modelos.Usuario> ListUsuarios { get; set; }
-#pragma warning restore CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
-            public int CurrentPage { get; set; }
-            public int TotalPages { get; set; }
-        }
+    public class UsuarioViewModel
+    {
+        public List<Modelos.Usuario> ListUsuarios { get; set; } = new List<Modelos.Usuario>();
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalRegistros { get; set; }
+        public int RegistrosPorPagina { get; set; }
+        public List<int> RegistrosPorPaginaOpciones { get; set; }
+    }
 
-    
+
+
 }
